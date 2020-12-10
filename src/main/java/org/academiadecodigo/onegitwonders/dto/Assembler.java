854bb0481f99d7d44407469b802a1aa8b0a6d6f6
nunosilva4 <nunosilvacmj@hotmail.com>
2020-12-10 20:19:02 +0000

    start assembler and rest

--- src/main/java/org/academiadecodigo/onegitwonders/dto/Assembler.java
+++ src/main/java/org/academiadecodigo/onegitwonders/dto/Assembler.java
@@ -1,5 +1,32 @@
 package org.academiadecodigo.onegitwonders.dto;
 
+import org.academiadecodigo.onegitwonders.model.Crew;
+import org.academiadecodigo.onegitwonders.model.Gangster;
+import org.academiadecodigo.onegitwonders.model.Mission;
+
 public class Assembler {
-    //TODO
+
+    public Gangster fromGangsterDto(GangsterDto gangsterDto) {
+        return null;
+    }
+
+    public Crew fromCrewDto(CrewDto crewDto) {
+        return null;
+    }
+
+    public Mission fromMissionDto(MissionDto missionDto) {
+        return null;
+    }
+
+    public GangsterDto toGangsterDto(Gangster gangster) {
+        return null;
+    }
+
+    public CrewDto toCrewDto(Crew crew) {
+        return null;
+    }
+
+    public MissionDto toMissionDto(Mission mission) {
+        return null;
+    }
 }
---
+++ src/main/java/org/academiadecodigo/onegitwonders/rest/Controller.java
@@ -0,0 +1,11 @@
+package org.academiadecodigo.onegitwonders.rest;
+
+import org.academiadecodigo.onegitwonders.dto.Assembler;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    private Assembler assembler;
+    //services
+}
